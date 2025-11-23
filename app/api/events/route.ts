8408e993ad772d86693ff589a538e7cbe291a678
Victor Berson <122553469+Dimebagou@users.noreply.github.com>
2025-11-23 13:13:39 +0100

    create first route for posting events

---
+++ app/api/events/route.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from "next/server";
+import connectDB from "@/lib/mongodb";
+import { Event } from "@/database";
+
+export async function POST(req: NextRequest) {
+    try {
+        await connectDB();
+
+        const formData = await req.formData();
+
+        let event;
+
+        try {
+            event = Object.fromEntries(formData.entries());
+        } catch {
+            return NextResponse.json(
+                {
+                    message: "Invalid JSON data format",
+                },
+                { status: 400 }
+            );
+        }
+
+        const createdEvent = await Event.create(event);
+
+        return NextResponse.json(
+            {
+                message: "Event Created Successfully",
+                event: createdEvent,
+            },
+            { status: 201 }
+        );
+    } catch (e) {
+        console.error(e);
+        return NextResponse.json(
+            {
+                message: "Event Creation Failed",
+                error: e instanceof Error ? e.message : "Unknown",
+            },
+            { status: 500 }
+        );
+    }
+}
