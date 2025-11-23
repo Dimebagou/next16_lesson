ada702a9501e9801be5511f1fe78e7968174ab91
Victor Berson <122553469+Dimebagou@users.noreply.github.com>
2025-11-23 14:03:43 +0100

    send uploaded image to cloudinary to store it

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,3 +1,4 @@
+import { v2 as cloudinary } from "cloudinary";
 import { NextRequest, NextResponse } from "next/server";
 import connectDB from "@/lib/mongodb";
 import { Event } from "@/database";
@@ -20,6 +21,34 @@ export async function POST(req: NextRequest) {
                 { status: 400 }
             );
         }
+
+        const file = formData.get("image") as File;
+
+        if (!file) {
+            return NextResponse.json(
+                {
+                    message: "Image file is required",
+                },
+                { status: 400 }
+            );
+        }
+
+        const arrayBuffer = await file.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+
+        const uploadResult = await new Promise((resolve, reject) => {
+            cloudinary.uploader
+                .upload_stream(
+                    { resource_type: "image", folder: "DevEvent" },
+                    (error, result) => {
+                        if (error) return reject(error);
+                        resolve(result);
+                    }
+                )
+                .end(buffer);
+        });
+
+        event.image = (uploadResult as { secure_url: string }).secure_url;
 
         const createdEvent = await Event.create(event);
 
